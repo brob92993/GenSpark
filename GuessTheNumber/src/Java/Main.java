b74db0beae1f6428cd9ba541e3e01d291f04b1da
Brian Robinson <brob92993@gmail.com>
2021-12-29 12:56:44 -0500

    updated guess the number to contain methods for unit testing

--- GuessTheNumber/src/Java/Main.java
+++ GuessTheNumber/src/Java/Main.java
@@ -1,69 +1,122 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-        Boolean play = true;
-        while (play) {
+    private int number, choice;
+    private String playAgain;
+    private String Name;
 
-            int min = 1;
-            int max = 20;
-            int a;
-            String s;
-            int tries = 0;
-            final int maxAttempts = 6;
+    public Main(){
 
-            Random r = new Random();
+    }
+
+    public String getName(){
+        return Name;
+    }
+    public void setName(String name){
+        this.Name = name;
+    }
+
+    //getter setter for playAgain option
+    public String getPlayAgain(){
+        return playAgain;
+    }
+    public void setPlayAgain(String playAgain) {
+        this.playAgain = playAgain;
+    }
 
 
-            System.out.println("Hello! What is your name?");
-            Scanner obj = new Scanner(System.in);
-            try {
-                s = obj.nextLine();
-                System.out.println("Well, " + s + ", I am thinking of a number between 1 & 20. Take a guess..");
+    public int getChoice(){
+        return choice;
+    }
+    public void setChoice(int choice) {
+        this.choice = choice;
+    }
+
+
+    public int getNumber(){
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    // set up
+    public void setUp(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hey there! What's your name?");
+        try {
+            setName( input.nextLine() );
+        }catch (Exception e){
+            System.out.println("name error, We will make your name BBB");
+            setName("BBB");
+        }
+    }
+
+    //make number between 1 and 20
+    private void createNumber(){
+        System.out.println("Well, " + getName() + " I'm thinking of a number between 1 and 20...");
+        Random random = new Random();
+        setNumber(random.nextInt(20)+1);
+    }
+
+    // game
+    public void game(){
+        int count = 0;
+
+        while(count<6){
+            System.out.println("Take a guess...");
+
+            setChoice( decision() );
+            if (getChoice() > getNumber() )
+                System.out.println("Your guess is too high. Lets try again");
+            if (getChoice() < getNumber() )
+                System.out.println("Your guess is too low. Lets try again");
+            if (getChoice() == getNumber() ) {
+                System.out.println("You got it " + getName() + "!");
+                System.out.println( "You guessed the right number in " + (count + 1) + " tries!");
+                return;
             }
-            catch (Exception e) {
-                System.out.println("Please enter a valid input");
-            }
-            int value = (int) (Math.random()*(max-min)) + min;
+            count++;
+        }
+        if(count == 6)
+            System.out.println("Out of tries! The correct number was " + getNumber() + "\n");
+    }
 
-            try {
-                do {
+    // player decision
+    public int decision(){
+        Scanner input = new Scanner(System.in);
+        try {
+            return input.nextInt();
+        }catch (InputMismatchException e) {
+            System.out.println("Not a number! Try again...");
+        } return 0;
+    }
 
-
-                    a = obj.nextInt();
-                    if (a == value) {
-                        System.out.println("You guessed it! The number was " + value);
-                        System.out.println("You guessed the number in " + tries + " tries");
-                        break;
-
-                    } else if (a < value) {
-                        ++tries;
-                        System.out.println("Your guess is too low. Take another guess.");
-                    } else if (a > value) {
-                        ++tries;
-                        System.out.println("Your guess is too high. Take another guess.");
-                    }
-                    if (tries == maxAttempts) {
-                        System.out.println("Out of tries! The number was " + value);
-                    }
+    public void playGame(){
+        Scanner input = new Scanner(System.in);
+        setPlayAgain("y");
 
 
-                } while (tries != maxAttempts);
-
-
-                System.out.println("Do you want to play again? (y/n)");
-                String answer = obj.next();
-                if (answer.equals("y")) {
-                    play = true;
-                } else {
-                    play = false;
-                }
-            }  catch (Exception e) {
-                System.out.println("Please enter a number");
-
+        while(getPlayAgain().equals("y")) {
+            setUp();
+            createNumber();
+            game();
+            System.out.println("Play again? (y or n)");
+            try{
+                setPlayAgain( input.nextLine().toLowerCase() );
+            }catch (Exception e) {
+                System.out.println("Enter y or n");
+                setPlayAgain("n");
             }
         }
-    }}
+    }
+
+
+    public static void main(String[] args) {
+        new Main().playGame();
+    }
+
+}
---
+++ GuessTheNumber/src/Test/MainTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import java.io.InputStream;
+
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+      
+    }
+
+    @Test
+    public void validUserInput_ShouldResultInExpectedOutput() {
+        String s = String.format("Brian",
+                    System.lineSeparator(),
+                    System.lineSeparator());
+        }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+}
