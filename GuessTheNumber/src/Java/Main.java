4d5f98778798627c82f2cd72d73c84f8b538c1c7
Brian Robinson <brob92993@gmail.com>
2021-12-29 11:25:59 -0500

    updated file structure to allow for unit tests in guess the number

---
+++ GuessTheNumber/src/Java/Main.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Boolean play = true;
+        while (play) {
+
+            int min = 1;
+            int max = 20;
+            int a;
+            String s;
+            int tries = 0;
+            final int maxAttempts = 6;
+
+            Random r = new Random();
+
+
+            System.out.println("Hello! What is your name?");
+            Scanner obj = new Scanner(System.in);
+            try {
+                s = obj.nextLine();
+                System.out.println("Well, " + s + ", I am thinking of a number between 1 & 20. Take a guess..");
+            }
+            catch (Exception e) {
+                System.out.println("Please enter a valid input");
+            }
+            int value = (int) (Math.random()*(max-min)) + min;
+
+            try {
+                do {
+
+
+                    a = obj.nextInt();
+                    if (a == value) {
+                        System.out.println("You guessed it! The number was " + value);
+                        System.out.println("You guessed the number in " + tries + " tries");
+                        break;
+
+                    } else if (a < value) {
+                        ++tries;
+                        System.out.println("Your guess is too low. Take another guess.");
+                    } else if (a > value) {
+                        ++tries;
+                        System.out.println("Your guess is too high. Take another guess.");
+                    }
+                    if (tries == maxAttempts) {
+                        System.out.println("Out of tries! The number was " + value);
+                    }
+
+
+                } while (tries != maxAttempts);
+
+
+                System.out.println("Do you want to play again? (y/n)");
+                String answer = obj.next();
+                if (answer.equals("y")) {
+                    play = true;
+                } else {
+                    play = false;
+                }
+            }  catch (Exception e) {
+                System.out.println("Please enter a number");
+
+            }
+        }
+    }}
