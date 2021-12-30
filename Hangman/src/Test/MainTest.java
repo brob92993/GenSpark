7abaf4b19b7b5ff075830219e4b785c2987cb122
Brian Robinson <brob92993@gmail.com>
2021-12-30 09:53:43 -0500

    added repo for hangman project with updated file structure

---
+++ Hangman/src/Test/MainTest.java
@@ -0,0 +1,17 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
